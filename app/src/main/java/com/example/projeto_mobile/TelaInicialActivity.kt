6b8c5e77d293703ad9f86bc1dd0740b34e2b8528
unknown <bran.oliv22@gmail.com>
2020-09-22 03:13:23 -0300

    Projeto Mobile

---
+++ app/src/main/java/com/example/projeto_mobile/TelaInicialActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projeto_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaInicialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_inicial)
+        //setSupportActionBar(toolbar)
+    }
+}
