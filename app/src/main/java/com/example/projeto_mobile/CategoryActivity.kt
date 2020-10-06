f9f30b1eb248937a5513b0ef00e82319bbf4ef40
ElijahLopp <eliaslopes657@gmail.com>
2020-10-05 22:34:28 -0300

    First commit

--- app/src/main/java/com/example/projeto_mobile/CategoryActivity.kt
+++ app/src/main/java/com/example/projeto_mobile/CategoryActivity.kt
@@ -6,20 +6,56 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_category2.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 @Suppress("DEPRECATION")
-class CategoryActivity : AppCompatActivity() {
+class CategoryActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category2)
-
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
         setSupportActionBar(toolbar_view)
 
         supportActionBar?.title = "Categorias"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        configuraMenuLateral()
+    }
+
+    private fun configuraMenuLateral () {
+        var toogle = ActionBarDrawerToggle(
+            this,
+            layoutMenuLateral,
+            toolbar_view,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close)
+        layoutMenuLateral.addDrawerListener(toogle)
+        toogle.syncState()
+        menu_lateral.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.categorias -> {
+                Toast.makeText(this, "Clicou Disciplinas", Toast.LENGTH_SHORT).show()
+            }
+            R.id.perfil -> {
+                Toast.makeText(this, "Clicou Disciplinas", Toast.LENGTH_SHORT).show()
+            }
+            R.id.ajuda -> {
+                Toast.makeText(this, "Clicou Disciplinas", Toast.LENGTH_SHORT).show()
+            }
+            R.id.sair -> {
+                Toast.makeText(this, "Clicou Disciplinas", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        layoutMenuLateral.closeDrawer(GravityCompat.START)
+        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
